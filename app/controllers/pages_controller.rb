e473079a3006dc72201584f875e10cfcfc89fda6
Amena <ame1.khatun@gmail.com>
2017-01-30 13:55:04 -0500

    adding different instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def welcome
-    render :welcome
+    @header = "This is the welcome page"
   end
   def about
+    @header = "this is the about page"
+  end
+  def contest
+    @header = "this is the contest page"
   end
 end
