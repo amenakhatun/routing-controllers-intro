1773e44eddab5476629694320a9dd4e165052a99
Amena <ame1.khatun@gmail.com>
2017-01-30 15:03:06 -0500

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#welcome'
   get '/welcome' => 'pages#welcome'
-  get '/' => 'pages#welcome'
-  get '/about' => 'pages#about'
-  get '/contest' => 'pages#contest'
-  get '/kitten/:size' => 'pages#kitten'
+  root 'pages#welcome'
+  root 'pages#about'
+  root 'pages#contest'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
   get '/secrets/:magic_word' => 'pages#secrets'
 
     # get '/kittens/:size' =>'pages#kittens'
