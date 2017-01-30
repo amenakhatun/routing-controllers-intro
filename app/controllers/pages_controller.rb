df79e94b10c8bd073962ca333c9fa1fac23f3f77
Amena <ame1.khatun@gmail.com>
2017-01-30 14:40:26 -0500

    making a secrets page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
    before_action :set_kitten_url, only: [:kitten, :kittens]
+
   def welcome
     @header = "This is the welcome page"
   end
+
   def about
     @header = "this is the about page"
   end
+
   def contest
     flash[:notice] = "Sorry, the contest has ended"
     redirect_to "/welcome"
@@ -20,5 +23,13 @@ class PagesController < ApplicationController
   def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+  def secrets
+    redirect_to "/welcome"
+    if params[:magic_word] == "help"
+    else flash[:alert] = "Sorry, you're not authorized to see that page!"
+
+    end
   end
 end
